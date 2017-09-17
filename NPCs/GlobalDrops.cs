12aa8c8df786beff43f7a213c30823a6b578f355
Ethan Womelsdorf <32006152+ETHANW0518@users.noreply.github.com>
2017-09-16 20:59:33 -0400

    Update GlobalDrops.cs

--- NPCs/GlobalDrops.cs
+++ NPCs/GlobalDrops.cs
@@ -17,3 +17,22 @@ namespace Gatekeeper.NPCs
         }
 	}
 }
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Gatekeeper.NPCs
+{
+	public class GlobalDrops : GlobalNPC
+	{
+		
+		public override void NPCLoot(NPC npc)  
+        {
+			if (npc.type == 509 && NPC.downedBoss2 && Main.rand.Next(3) == 1)
+			{
+				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("SwarmerHide"), 1);
+			}
+
+        }
+	}
+}
