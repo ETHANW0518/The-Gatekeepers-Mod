69cb5ce4a86f8e857dc0fd9e53ba7ae3fb8800ee
Ethan Womelsdorf <32006152+ETHANW0518@users.noreply.github.com>
2017-09-16 20:42:54 -0400

    Create SwarmerHide.cs

---
+++ Items/SwarmerHide.cs
@@ -0,0 +1,21 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Gatekeeper.Items
+{
+	public class SwarmerHide : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Swarmer Hide");
+			Tooltip.SetDefault("'The hide feels as strong as oak'");
+		}
+		public override void SetDefaults()
+			item.width = 20;
+			item.height = 14;
+			item.value = 5;
+			item.rare = 2;
+			item.maxStack = 999;
+		}
+	}
+}
