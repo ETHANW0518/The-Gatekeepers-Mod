2604c7a954f33f83d2dc92c16ee45f113af75a16
Ethan Womelsdorf <32006152+ETHANW0518@users.noreply.github.com>
2017-09-18 10:53:24 -0400

    Update GlobalNPCDrops.cs

--- NPCs/GlobalNPCDrops.cs
+++ NPCs/GlobalNPCDrops.cs
@@ -11,13 +11,16 @@ namespace Gatekeeper.NPCs
         {
 			if (npc.type == 537 && NPC.downedBoss2 && Main.rand.Next(3) == 1)
 			{
-				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("SandyGel"), 1);
+				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("SandyGel"), 1); 1);
 			}
 			if (npc.type == 509 && NPC.downedBoss2 && Main.rand.Next(3) == 1)
             {
                 Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("SwarmerHide"), 1);
-            }
-
+           		}
+	   		if (npc.type == 61 && NPC.downedBoss2 && Main.rand.Next(3) == 1)
+	    {
+				Item.NewItem((int)npc.position.x, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("VultureEye"), 1);
+			}
         }
 	}
 }
